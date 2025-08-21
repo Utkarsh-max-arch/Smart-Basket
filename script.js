bba89eecd8ef4ccebd1f23ab6d60b7a3ed84cafe
Utkarsh Singh <us800906@gmail.com>
2025-08-21 20:07:55 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,76 @@
+// let add slider for that
+$('.item-slider').slick({
+  dots: false,
+  infinite: true,
+  arrow:false,
+  speed: 300,
+  slidesToShow: 8,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+       
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.product-slider').slick({
+  dots: false,
+  infinite: true,
+  arrow:false,
+  speed: 300,
+  slidesToShow: 8,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+       
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+
+				
